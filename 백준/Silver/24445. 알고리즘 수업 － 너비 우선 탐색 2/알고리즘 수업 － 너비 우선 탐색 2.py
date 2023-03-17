9e5ed3bd5aefbb8b2d84ac12c0ad56769539fd63
JangWooSeok <wowseok2774@gmail.com>
2023-03-17 18:50:31 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 712 ms, Memory: 65184 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,36 @@
+import sys
+from collections import defaultdict
+from collections import deque 
+
+input = sys.stdin.readline
+
+def bfs(graph, start):
+    cnt = 1
+    visited = [0] * (N+1)
+    need_visited = deque()
+    need_visited.append(start)
+    
+    while need_visited:
+        node = need_visited.popleft()
+        if visited[node]  == 0:
+            visited[node] = cnt
+            need_visited.extend(graph[node])
+            cnt+=1
+    return visited
+    
+
+if __name__ == "__main__":
+    N, M, R = map(int, input().split())
+   
+    graph = defaultdict(list)
+    for _ in range(M):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+        
+    for i in range(1, N+1):
+        graph[i].sort(reverse = True)
+
+    answer = bfs(graph, R)
+    for i in range(1, N+1):
+        print(answer[i])
