0c94731699a95ec9f610b144386f6b4de310ca11
JangWooSeok <wowseok2774@gmail.com>
2023-04-15 16:35:39 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,8 @@
+school_dict = {
+        "NLCS": "North London Collegiate School",
+        "BHA": "Branksome Hall Asia",
+        "KIS": "Korea International School",
+        "SJA": "St. Johnsbury Academy"}
+
+school_name = input()
+print(school_dict[school_name])
